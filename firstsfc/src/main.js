4354013727ba2936dd69d8bcbfca794931eb2822
Leonard Delos Reyes <ldelosreyes@student.apc.edu.ph>
2026-01-15 22:24:13 +0800

    Proper naming

--- firstsfc/src/main.js
+++ firstsfc/src/main.js
@@ -9,6 +9,6 @@ import PersonalProfile from './components/PersonalProfile.vue'
 const app = createApp(App)
 app.component('food-item', FoodItem)
 app.component('food-item2', FoodItem2)
-app.component('Personal-Profile', PersonalProfile)
+app.component('personal-profile', PersonalProfile)
 
 app.mount('#app')
