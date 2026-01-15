780e8a4fefb93a0a30d5619a60107e1148532e44
Leonard Delos Reyes <ldelosreyes@student.apc.edu.ph>
2026-01-15 15:29:56 +0800

    Added PersonalProfile

---
+++ firstsfc/src/components/PersonalProfile.vue
@@ -0,0 +1,36 @@
+<template>
+<div class="container">
+  <div>About Me <br>
+</div>
+  <div>Education / Achievements <br>
+          <ul>
+            <li>2nd Year College Student</li>
+            <li>1st Year with Honors (2024–2025)</li>
+            <li>Grade 10 with Honors (2020–2021)</li>
+          </ul>
+</div>
+  <div>Course <br>
+     - Bachelor of Science in Information Technology <br>
+  </div>  
+  <div> IT Experience <br>
+          <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>Java</li>
+            <li>Python</li>
+          </ul>
+  <div>Hobbies & Interests <br>
+          <ul>
+            <li>Playing online games</li>
+            <li>Watching anime</li>
+            <li>Reading manga & manhwa</li>
+            <li>Working out</li>
+          </ul>
+  </div>
+  <div>Goals In Life / Dream <br>
+  to be financially stable with a peaceful life <br>
+  </div>  
+  <div>Picture Gallery <br>
+  
+<script></script>
+<style></style>
