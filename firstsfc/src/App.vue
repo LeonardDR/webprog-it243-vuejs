5c9993d2732f6fb08dbba08081abbbf2e925c96f
Leonard Delos Reyes <ldelosreyes@student.apc.edu.ph>
2026-01-15 22:22:05 +0800

    Added Personal Profile

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,10 +1,14 @@
 <template>
-  <h1>Food</h1>
-  <Personal-Profile/>
-  <food-item/>
-  <food-item2/> 
+  <div>
+    <h1>Food</h1>
+    <personal-profile />
+    <food-item />
+    <food-item2 />
+  </div>
 </template>
 
-<script></script>
+<script>
+export default {}
+</script>
 
 <style></style>
--- firstsfc/src/components/PersonalProfile.vue
+++ firstsfc/src/components/PersonalProfile.vue
@@ -1,38 +1,60 @@
 <template>
-<div class="container">
-  <div>About Me <br>
-</div>
-  <div>Education / Achievements <br>
-          <ul>
-            <li>2nd Year College Student</li>
-            <li>1st Year with Honors (2024–2025)</li>
-            <li>Grade 10 with Honors (2020–2021)</li>
-          </ul>
-</div>
-  <div>Course <br>
-     - Bachelor of Science in Information Technology <br>
-  </div>  
-  <div> IT Experience <br>
-          <ul>
-            <li>HTML</li>
-            <li>CSS</li>
-            <li>Java</li>
-            <li>Python</li>
-          </ul>
-  <div>Hobbies & Interests <br>
-          <ul>
-            <li>Playing online games</li>
-            <li>Watching anime</li>
-            <li>Reading manga & manhwa</li>
-            <li>Working out</li>
-          </ul>
+  <div class="container">
+    <div>
+      <strong>About Me</strong><br />
+      I am an IT student who enjoys learning technology.
+    </div>
+
+    <div>
+      <strong>Education / Achievements</strong>
+      <ul>
+        <li>2nd Year College Student</li>
+        <li>1st Year with Honors (2024–2025)</li>
+        <li>Grade 10 with Honors (2020–2021)</li>
+      </ul>
+    </div>
+
+    <div>
+      <strong>Course</strong><br />
+      Bachelor of Science in Information Technology
+    </div>
+
+    <div>
+      <strong>IT Experience</strong>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>Java</li>
+        <li>Python</li>
+      </ul>
+    </div>
+
+    <div>
+      <strong>Hobbies & Interests</strong>
+      <ul>
+        <li>Playing online games</li>
+        <li>Watching anime</li>
+        <li>Reading manga & manhwa</li>
+        <li>Working out</li>
+      </ul>
+    </div>
+
+    <div>
+      <strong>Goals In Life / Dream</strong><br />
+      To be financially stable with a peaceful life
+    </div>
+
+    <div>
+      <strong>Picture Gallery</strong><br />
+      [ Image Placeholder ]
+    </div>
   </div>
-  <div>Goals In Life / Dream <br>
-  to be financially stable with a peaceful life <br>
-  </div>  
-  <div>Picture Gallery <br>
-  
 </template>
 
-<script></script>
+<script>
+export default {
+  name: 'PersonalProfile'
+}
+</script>
+
 <style></style>
